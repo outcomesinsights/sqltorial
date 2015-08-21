fa5c4e6f3ae2d5a0c27f27d2c085137b64fcdda1
Ryan Duryea <aguynamedryan@gmail.com>
2015-08-21 11:52:27 -0700

    Update README and metadata to transfer to Outcomes Insights

--- README.md
+++ README.md
@@ -54,7 +54,7 @@ To install this gem onto your local machine, run `bundle exec rake install`. To 
 
 ## Contributing
 
-1. Fork it ( https://github.com/aguynamedryan/sqltorial/fork )
+1. Fork it ( https://github.com/outcomesinsights/sqltorial/fork )
 2. Create your feature branch (`git checkout -b my-new-feature`)
 3. Commit your changes (`git commit -am 'Add some feature'`)
 4. Push to the branch (`git push origin my-new-feature`)
--- lib/sqltorial/metadata.rb
+++ lib/sqltorial/metadata.rb
@@ -1,6 +1,6 @@
 module SQLtorial
   VERSION = "0.0.1"
   SUMMARY = %q{Knitr, but for SQL files, sorta}
-  DESCRIPTION = %q{Ingests a set of comments SQL statements, executes them, and dumps queries and results into a markdown file}
-  HOMEPAGE = "http://github.com/aguynamedryan/sqltorial"
+  DESCRIPTION = %q{Ingests a set of commented SQL statements, executes them, and dumps the comments, queries, and results into a markdown file}
+  HOMEPAGE = "http://github.com/outcomesinsights/sqltorial"
 end
